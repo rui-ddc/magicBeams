26ad218a907a9d9337aabcc67af35fc558bb8618
David <rui-ddc@fct.unl.pt>
2026-04-27 21:17:16 +0100

    project setup

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        while (input.hasNext()) {
+            System.out.println(input.nextLine());
+        } 
+    }
+}
