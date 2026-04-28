5f0bd9bbae03a567e538ef84c86e3d6c817d539e
David <rui-ddc@fct.unl.pt>
2026-04-28 20:52:00 +0100

    build occupied matrix, check beams on chosen columns, check for false alarm, beam ray tracing

--- Main.java
+++ Main.java
@@ -1,9 +1,88 @@
 import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 
 class Main {
     record Beam(int r, int c, int l, char d) {}
 
     private static String solve(int R, int C, int N, int L, int B, Beam[] beams) {
+
+        int[][] occupied = new int[R][C];
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                occupied[r][c] = -1;
+            }
+        }
+
+        for (int i = 0; i < beams.length; i++) {
+            for (int l = 0; l < beams[i].l(); l++) {
+                if (beams[i].d() == 'N') {
+                    occupied[beams[i].r() - l][beams[i].c()] = i;
+                } else if (beams[i].d() == 'S') {
+                    occupied[beams[i].r() + l][beams[i].c()] = i;
+                } else if (beams[i].d() == 'E') {
+                    occupied[beams[i].r()][beams[i].c() + l] = i;
+                } else if (beams[i].d() == 'W') {
+                    occupied[beams[i].r()][beams[i].c() - l] = i;
+                }
+            }
+        }
+
+        if (true) {
+            // Grid display for debugging
+            for (int row = 0; row < occupied.length; row++) {
+                for (int col = 0; col < occupied[row].length; col++) {
+                    System.out.printf("%4d", occupied[row][col]); // width = 4 chars
+                }
+                System.out.println();
+            }
+        }
+
+        // Check for necessary beams to move
+        Set<Integer> beamsInTheWay = new HashSet<>();
+        for (int c = L; c < L + N; c++) {
+            for (int r = 0; r < R; r++) {
+                if (occupied[r][c] > -1) {
+                    beamsInTheWay.add(occupied[r][c]);
+                }
+            }
+        }
+        // If none return Flase Alarm
+        if (beamsInTheWay.isEmpty()) {
+            return "False alarm";
+        }
+
+        // Ray tracing
+        for (int i : beamsInTheWay) {
+            if (beams[i].d() == 'N') {
+                System.out.printf("beam %d: %c ", i, beams[i].d());
+                for (int r = beams[i].r() - beams[i].l(); r > -1; r--) {
+                    System.out.printf("%4d", occupied[r][beams[i].c()]);
+                }
+                System.out.println();
+            } else if (beams[i].d() == 'S') {
+                System.out.printf("beam %d: %c ", i, beams[i].d());
+                for (int r = beams[i].r() + beams[i].l(); r < R; r++) {
+                    System.out.printf("%4d", occupied[r][beams[i].c()]);
+                }
+                System.out.println();
+            } else if (beams[i].d() == 'E') {
+                System.out.printf("beam %d: %c ", i, beams[i].d());
+                for (int c = beams[i].c() + beams[i].l(); c < C; c++) {
+                    System.out.printf("%4d", occupied[beams[i].r()][c]);
+                }
+                System.out.println();
+
+            } else if (beams[i].d() == 'W') {
+                System.out.printf("beam %d: %c ", i, beams[i].d());
+                for (int c = beams[i].c() - beams[i].l(); c > -1; c--) {
+                    System.out.printf("%4d", occupied[beams[i].r()][c]);
+                }
+                System.out.println();
+            }
+        }
+
         /*
             #1 Check which beams intercept the chosen columns
                 #1.1 If no beams intercept out> "False alarm"
